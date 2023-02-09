96e28258241260450967284781bc800b8b612040
Leonardo Anzulin <leoanzulin@gmail.com>
2023-02-09 11:30:38 -0300

    INITIAL COMMIT

---
+++ src/pages/Projects.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from '../components/Header'
+
+class Projects extends React.Component {
+  render() {
+    return (
+        <div>
+            <Header/>
+            <h1>Projects</h1>
+        </div>
+    );
+  }
+}
+
+export default Projects;
