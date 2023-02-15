df7cb7fae88ea30f6f4131067e5cae2c918d4660
Leonardo Anzulin <leoanzulin@gmail.com>
2023-02-15 07:55:37 -0300

    CSS Inicial

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import Header from '../components/Header'
+import './Contact.css'
 
 class Contact extends React.Component {
   render() {
     return (
         <div>
             <Header/>
-            <h1>Contact</h1>
+            <div className='contact'>
+              <h1>Contact</h1>
+            </div>
         </div>
     );
   }
