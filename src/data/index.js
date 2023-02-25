b42d768307938e999b6fe9567c5cbab945282218
Leonardo Anzulin <leoanzulin@gmail.com>
2023-02-24 22:58:01 -0300

    Finaliza Pagina de Projetos

---
+++ src/data/index.js
@@ -0,0 +1,13 @@
+const projects = [
+    {
+        id :1,
+        name: 'Trybe-Wallet',
+        image: require('../image/trybeWallet.png'),
+        description: 'Projeto Trybe-Wallet é uma carteira virtual que permite adicionar despesas em diversas moedas e converte para reais, mostrando a soma total de todos os gastos adicionados. ',
+        deploy: 'https://trybe-wallet-ruddy.vercel.app/',
+        usedStacks: 'React, API, CSS',        
+        repository: 'https://github.com/leoanzulin/Trybe-Wallet',
+    },
+];
+
+export default projects
